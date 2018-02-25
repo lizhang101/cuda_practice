dcff25a72a42636c41815d24625c401aa564628b
asiclzscl@gmail.com <asiclzscl@gmail.com>
2018-02-24 21:24:30 -0800

    shm mm

--- basic/reduce/reduce.cu
+++ basic/reduce/reduce.cu
@@ -18,9 +18,31 @@ __global__ void sumSingleBlock(int *d)
     }
 }
 
+__global__ void sumSingleBlock_shm(int *d)
+{
+    extern __shared__ int dcopy[];
+    int tid = threadIdx.x;
+
+    dcopy[tid*2] = d[tid*2];
+    dcopy[tid*2+1] = d[tid*2+1];
+    for (int tc = blockDim.x, stepSize = 1; tc > 0; tc >>=1, stepSize <<= 1)
+    {
+        if (tid < tc)
+        {
+            int pa = tid * stepSize * 2;
+            int pb = pa + stepSize;
+            dcopy[pa] += dcopy[pb];
+        }
+    }
+    if (tid == 0) 
+    {
+        d[0] = dcopy[0];
+    }
+}
+
 int main()
 {
-    const int count = 512;
+    const int count = 32;
     const size_t size = count * sizeof(int);
     int h[count];
     for (int i=0; i<count; ++i)
@@ -36,6 +58,14 @@ int main()
 
     int result;
     cudaMemcpy(&result, d, sizeof(int), cudaMemcpyDeviceToHost);
+    //cudaFree(d);
+    std::cout << "use global mem:" << result << std::endl;
+
+    cudaMemcpy(d, h, size, cudaMemcpyHostToDevice);
+    sumSingleBlock_shm<<<1, count/2, count>>>(d);
+
+    cudaMemcpy(&result, d, sizeof(int), cudaMemcpyDeviceToHost);
+    std::cout << "use shared mem:" << result << std::endl;
+
     cudaFree(d);
-    std::cout << result << std::endl;
 }
